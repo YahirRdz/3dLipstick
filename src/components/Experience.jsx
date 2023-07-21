f5bec0808a048d1e51bd83d5c046e7b692c7113d
YayoRdz <yajosh03@gmail.com>
2023-07-21 11:43:57 -0600

    update project

---
+++ src/components/Experience.jsx
@@ -0,0 +1,22 @@
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
+
+export const Experience = () => {
+  const map = useTexture("/textures/office.jpg");
+  const viewport = useThree((state) => state.viewport);
+
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
+      <OrbitControls />
+      <Avatar position={[0, -4, 5]} scale={3} />
+      <mesh>
+        <sphereGeometry args={[10, 64, 64]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
+      </mesh>
+    </>
+  );
+};
